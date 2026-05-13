bd82b614b3f1e60cc9ad908df479603f2e1c4e40
Isaac Adjei <offices.isaac@gmail.com>
2026-05-13 03:04:50 +0100

    fix: add Aston University SVG favicon to all pages

--- index.php
+++ index.php
@@ -60,6 +60,7 @@ sort($languages);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AstonCV - Browse CVs</title>
+    <link rel="icon" type="image/svg+xml" href="images/logo.svg">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
